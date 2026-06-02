90367228ea1cf24c0c843ab021e6b483d92a8dad
aaraf097980-collab <aaraf097980@gmail.com>
2026-06-02 21:45:09 +0530

    Added Day 2 MaximumSubarray Solution

---
+++ Week_1/Day_2/MaximumSubarray.java
@@ -0,0 +1,27 @@
+package Week_1.Day_2;
+  
+public class MaximumSubarray {
+    class Solution {
+         public int maxSubArray(int[] nums) {
+    int sum=0;
+    int maxi=Integer.MIN_VALUE;
+    for (int i=0;i<nums.length;i++){
+        sum=sum + nums[i];
+        if (sum>maxi){
+            maxi=sum;
+        }
+        if (sum<0){
+            sum=0;
+        }
+    }
+    return maxi;
+}
+}
+     public static void main (String []args){
+       MaximumSubarray obj=new MaximumSubarray();
+       int [] nums= {5,4,-1,7,8};
+      int result = obj.new Solution().maxSubArray(nums);
+       System.out.println(result); 
+     }
+  
+    }
