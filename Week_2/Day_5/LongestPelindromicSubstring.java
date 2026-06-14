5e9a5b901e6f5b58908a78b1e340b688d4d714fc
aaraf097980-collab <aaraf097980@gmail.com>
2026-06-14 22:47:28 +0530

    Added Week_2 Day_5: Longest Pelindromic Substring

---
+++ Week_2/Day_5/LongestPelindromicSubstring.java
@@ -0,0 +1,37 @@
+package Week_2.Day_5;
+
+public class LongestPelindromicSubstring {
+     public String longestPalindrome(String s) {
+        // Edge case: agar string null ya empty hai
+      if (s ==null || s.length()<1) return "";
+      int start =0;
+      int end =0;
+      for (int i= 0;i<s.length();i++){
+        // odd length pelindrome 
+        int len1=expand(s , i, i);
+        // even length pelindrome 
+        int len2=expand(s,i,i+1);
+        int len =Math.max(len1,len2);
+        //Agar current palindrome bada hai to start/end update karo
+        if (len>end-start){
+            start=i-(len-1)/2;
+            end=i+len/2;
+        }
+      }
+      // Longest palindrome substring return karo
+      return s.substring(start,end+1);
+    }
+    private int expand(String  s,int left ,int right){
+     while (left>=0 && right < s.length() && s.charAt(left)==s.charAt(right)){
+        left--;
+        right++;
+     }
+     return right-left-1;
+    }
+    public static void main (String [] args){
+        LongestPelindromicSubstring obj =new LongestPelindromicSubstring();
+        String s="cababbd";
+        System.out.println(obj.longestPalindrome(s));
+    }
+    
+}
