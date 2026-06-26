f9ac98739a8589e3e7d733fce35b4f9f31ad8878
aaraf097980-collab <aaraf097980@gmail.com>
2026-06-26 22:27:59 +0530

    jkjj

---
+++ Week_3/Day_4/LargestRectangleInHistogram.java
@@ -0,0 +1,27 @@
+package Week_3.Day_4;
+
+import java.util.Stack;
+
+public class LargestRectangleInHistogram {
+     public int largestRectangleArea(int[] heights) {
+       Stack<Integer> stack=new Stack<>();
+       int n=heights.length;
+       int max=Integer.MIN_VALUE;
+       for (int i=0;i<=n;i++){
+        int element=(i==n) ? 0:heights[i];
+        while(!stack.isEmpty() && heights[stack.peek()]>element){
+            int h=heights[stack.pop()];
+            int ps=(stack.isEmpty()) ? -1: stack.peek();
+            int w=i-ps-1;
+            max=Math.max(max,h*w);
+        }
+        stack.push(i);
+       }
+       return (max==Integer.MIN_VALUE) ?0:max;
+    }
+    public static void main(String [] args){
+        LargestRectangleInHistogram  obj=new LargestRectangleInHistogram();
+        int[] heights = {2,1,5,6,2,3};
+        System.out.println(obj.largestRectangleArea(heights));
+    }
+}
